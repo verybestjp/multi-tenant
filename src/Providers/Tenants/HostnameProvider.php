8163e210314da21f06dec1fd12f214bd47ab4806
Junya Namai <junamai2000@yahoo.co.jp>
2026-04-02 15:25:42 +0900

    Revert "HostnameProvider修正"
    This reverts commit 4087404577608a1180ee23f72ea4d638662f2f00.

--- src/Providers/Tenants/HostnameProvider.php
+++ src/Providers/Tenants/HostnameProvider.php
@@ -23,16 +23,6 @@ use Illuminate\Contracts\Support\DeferrableProvider;
 
 class HostnameProvider extends ServiceProvider implements DeferrableProvider
 {
-    public function register()
-    {
-        $empty = function () {
-            return null;
-        };
-
-        $this->app->singleton(CurrentHostname::class, $empty);
-        $this->app->singleton(Tenant::class, $empty);
-    }
-
     public function provides()
     {
         return [
