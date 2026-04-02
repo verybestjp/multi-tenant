d4ff40ae3626fa3d01d337f598bccce546ee182b
Junya Namai <junamai2000@yahoo.co.jp>
2026-04-02 10:55:10 +0900

    l13 compatibility

--- src/Environment.php
+++ src/Environment.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the hyn/multi-tenant package.
  *
- * (c) Daniël Klabbers <daniel@klabbers.email>
+ * (c) Dani谷l Klabbers <daniel@klabbers.email>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
